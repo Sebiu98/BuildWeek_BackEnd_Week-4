ad2f5ea60e71d974568a72eec5645da3a524ad24
val-mai <valerio.maiolini@gmail.com>
2022-08-08 15:27:00 +0200

    jpaUtil

--- BuildWeek_BackEnd_Week-4/src/it/epicode/be/trasporti/model/Utente.java
+++ BuildWeek_BackEnd_Week-4/src/it/epicode/be/trasporti/model/Utente.java
@@ -1,6 +1,10 @@
 package it.epicode.be.trasporti.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Utente {
+	@Id
 	private String nome;
-  //ciao a tutti
 }
