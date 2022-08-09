66d62df784991446a4b6064fd0174d9e9a0cbabf
Eusebiu Ciobanu <sebiu.ciobanu98@gmail.com>
2022-08-09 11:28:31 +0200

    creazione classi abbonamento, biglietto e classe astratta per i due

--- BuildWeek_BackEnd_Week-4/src/it/epicode/be/trasporti/model/Abbonamento.java
+++ BuildWeek_BackEnd_Week-4/src/it/epicode/be/trasporti/model/Abbonamento.java
@@ -1,6 +1,44 @@
 package it.epicode.be.trasporti.model;
 
-public class Abbonamento {
+import java.time.LocalDate;
+import javax.persistence.Entity;
 
-	//sdfsadfdsaf
+@Entity
+public class Abbonamento extends TitoliViaggio {
+
+	private DurataAbbonamento durataAbonamento;
+
+	private Utente utente;
+
+	public Abbonamento(Integer id, LocalDate dataInizio, double prezzo, DurataAbbonamento durataAbonamento,
+			Utente utente) {
+		super(id, dataInizio, prezzo);
+		this.durataAbonamento = durataAbonamento;
+		this.utente = utente;
+	}
+
+	public Abbonamento() {
+	}
+
+	public DurataAbbonamento getDurataAbonamento() {
+		return durataAbonamento;
+	}
+
+	public void setDurataAbonamento(DurataAbbonamento durataAbonamento) {
+		this.durataAbonamento = durataAbonamento;
+	}
+
+	public Utente getUtente() {
+		return utente;
+	}
+
+	public void setUtente(Utente utente) {
+		this.utente = utente;
+	}
+
+	@Override
+	public String toString() {
+		return "Abbonamento [durataAbonamento=" + durataAbonamento + ", utente=" + utente + "]";
+	}
+
 }
--- BuildWeek_BackEnd_Week-4/src/it/epicode/be/trasporti/model/Biglietto.java
+++ BuildWeek_BackEnd_Week-4/src/it/epicode/be/trasporti/model/Biglietto.java
@@ -1,5 +1,30 @@
 package it.epicode.be.trasporti.model;
 
-public class Biglietto {
+import java.time.LocalDate;
+
+public class Biglietto extends TitoliViaggio {
+
+	private boolean status;
+
+	public Biglietto(Integer id, LocalDate dataInizio, double prezzo, boolean status) {
+		super(id, dataInizio, prezzo);
+		this.status = status;
+	}
+
+	public Biglietto() {
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Biglietto [status=" + status + "]";
+	}
 
 }
